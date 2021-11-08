b7c5f241dd9435bfb995a9374f7127acc19279df
DavidCasc <50248611+DavidCasc@users.noreply.github.com>
2021-11-08 18:00:18 -0500

    Added login connectivity

--- app/src/main/java/com/example/cardiaccorner/SignInActivity.java
+++ app/src/main/java/com/example/cardiaccorner/SignInActivity.java
@@ -2,12 +2,22 @@ package com.example.cardiaccorner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.text.TextUtils;
 import android.view.View;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class SignInActivity extends AppCompatActivity {
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class   SignInActivity extends AppCompatActivity {
+
+    EditText username, password;
+    Button btnLogin;
+
 
 
     @Override
@@ -15,5 +25,45 @@ public class SignInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signin_screen);
 
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        btnLogin = findViewById(R.id.sign_in);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(TextUtils.isEmpty(username.getText().toString()) || TextUtils.isEmpty(password.getText().toString())){
+                    Toast.makeText(SignInActivity.this, "Username / Password Required", Toast.LENGTH_LONG).show();
+                } else {
+                    login();
+                }
+
+            }
+        });
+    }
+    public void login() {
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setUsername(username.getText().toString());
+        loginRequest.setPassword(password.getText().toString());
+
+        Call<LoginResponse> loginResponseCall = ApiClient.getUserService().userLogin(loginRequest);
+
+
+        loginResponseCall.enqueue(new Callback<LoginResponse>() {
+            @Override
+            public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+                if(response.isSuccessful()){
+                    Toast.makeText(SignInActivity.this, "Login Successfull", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(SignInActivity.this, "Login Failed", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<LoginResponse> call, Throwable t) {
+                Toast.makeText(SignInActivity.this, "Throwable: " + t.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
