737ac4cd8faccc497cde693b7d0f7670edc6ec9f
lorayzy <93155835+lorayzy@users.noreply.github.com>
2021-11-17 19:55:57 -0500

    navigation for authentication

--- app/src/main/java/com/example/cardiaccorner/NewMeasurementActivity.java
+++ app/src/main/java/com/example/cardiaccorner/NewMeasurementActivity.java
@@ -8,10 +8,16 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.material.chip.Chip;
+
 public class NewMeasurementActivity extends AppCompatActivity {
 
     String systolic_var = "120"; //temp until we have measurement from monitor passed in
     String diastolic_var = "80"; //temp until we have measurement from monitor passed in
+
+    Chip chip1;
+    Chip chip2;
+    Chip chip3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
