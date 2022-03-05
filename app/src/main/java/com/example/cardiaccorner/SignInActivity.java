0377fee00bc6cf875bcc511176021164c66c4011
DavidCasc <50248611+DavidCasc@users.noreply.github.com>
2022-03-04 20:40:54 -0500

    Added a years worth of dates in on signing in

--- app/src/main/java/com/example/cardiaccorner/SignInActivity.java
+++ app/src/main/java/com/example/cardiaccorner/SignInActivity.java
@@ -16,6 +16,10 @@ import retrofit2.Response;
 
 import android.content.SharedPreferences;
 
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+
 public class  SignInActivity extends AppCompatActivity {
 
     EditText username, password;
@@ -29,7 +33,14 @@ public class  SignInActivity extends AppCompatActivity {
         editor.putString(Key, Val);
         editor.commit();
     }
-
+    public void storeLogs(ArrayList<Entry> log){
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String arr = gson.toJson(log);
+        editor.putString("logs", arr);
+        editor.commit();
+    }
     private void signIn(){
         SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
@@ -94,6 +105,16 @@ public class  SignInActivity extends AppCompatActivity {
                     saveData("username", username.getText().toString());
                     saveData("password", password.getText().toString());
                     saveData("email", response.body().getEmail());
+
+                    ArrayList<Entry> log = new ArrayList<>();
+                    log.add(new Entry("23-06-2021 16:23:07", 120, 80, true, false, false, "Had a good workout", true));
+                    log.add(new Entry("30-06-2021 11:23:07", 125, 90, false, false, true, "Had a stressful meeting", true));
+                    log.add(new Entry("20-07-2021 19:23:07", 130, 95, false, true, true, "Meeting with the parents", true));
+                    log.add(new Entry("21-10-2021 23:23:07", 150, 100, true, true, false, "Good workout", true));
+                    log.add(new Entry("23-10-2021 10:23:07", 110, 60, false, false, true, "Bad exam", true));
+                    log.add(new Entry("25-10-2021 9:23:07", 115, 75, false, true, true, "Family Dinner", true));
+                    log.add(new Entry("01-01-2022 12:23:07", 121, 84, true, false, false, "New Year resolution at the gym", true));
+                    storeLogs(log);
                     Intent i = new Intent(SignInActivity.this,MainActivity.class);
                     startActivity(i);
                 } else {
