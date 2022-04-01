e8733cc6bee5b2e42641316017a4c2e2aae14737
DavidCasc <50248611+DavidCasc@users.noreply.github.com>
2022-04-01 18:00:47 -0400

    Added documentation to the ApiClient and AuthClient

--- app/src/main/java/com/example/cardiaccorner/ApiClient.java
+++ app/src/main/java/com/example/cardiaccorner/ApiClient.java
@@ -7,23 +7,30 @@ import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+/**
+ * The ApiClient Class is used to build a user service with
+ * Retrofit2. This is used to make HTTP calls to the web API
+ * Data server
+ */
 public class ApiClient {
     private static Retrofit getRetrofit(){
 
+        //This is used for debugging the HTTP communication between the client and the API
         HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
         httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
+        //This will build the HTTP client to make the call
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
                 .addInterceptor(httpLoggingInterceptor)
-                .connectTimeout(10, TimeUnit.SECONDS)
-                .writeTimeout(10, TimeUnit.SECONDS)
-                .readTimeout(30, TimeUnit.SECONDS)
+                .connectTimeout(10, TimeUnit.SECONDS)   //If it cannot connect to the server in 10 seconds timeout
+                .writeTimeout(10, TimeUnit.SECONDS)     //If it cannot write to the server in 10 seconds timeout
+                .readTimeout(30, TimeUnit.SECONDS)      //If it does not get a response in 30 seconds timeout
                 .build();
 
-
+        //Create the Retrofit handler
         Retrofit retrofit = new Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create())
-                .baseUrl("http://192.168.2.69:3003")
+                .baseUrl("http://192.168.2.69:3003") //Change to the address of the API Server
                 .client(okHttpClient)
                 .build();
 
@@ -31,6 +38,7 @@ public class ApiClient {
 
     }
 
+    //Get all the calls that will be made through the HTTP Client
     public static UserService getUserService() {
         UserService userService = getRetrofit().create(UserService.class);
 
--- app/src/main/java/com/example/cardiaccorner/AuthClient.java
+++ app/src/main/java/com/example/cardiaccorner/AuthClient.java
@@ -6,31 +6,37 @@ import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-
+/**
+ * The ApiClient Class is used to build a user service with
+ * Retrofit2. This is used to make HTTP calls to the web API
+ *  Authentication Server
+ */
 public class AuthClient {
     private static Retrofit getRetrofit(){
 
+        //This is used for debugging the HTTP communication between the client and the API
         HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
         httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
+        //This will build the HTTP client to make the call
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
                 .addInterceptor(httpLoggingInterceptor)
-                .connectTimeout(10, TimeUnit.SECONDS)
-                .writeTimeout(10, TimeUnit.SECONDS)
-                .readTimeout(30, TimeUnit.SECONDS)
+                .connectTimeout(10, TimeUnit.SECONDS)   //If it cannot connect to the server in 10 seconds timeout
+                .writeTimeout(10, TimeUnit.SECONDS)     //If it cannot write to the server in 10 seconds timeout
+                .readTimeout(30, TimeUnit.SECONDS)      //If it does not get a response in 30 seconds timeout
                 .build();
 
-
+        //Create the Retrofit handler
         Retrofit retrofit = new Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create())
-                .baseUrl("http://192.168.2.69:3001")
+                .baseUrl("http://192.168.2.69:3001") //Change to the address of the API Server
                 .client(okHttpClient)
                 .build();
 
         return retrofit;
 
     }
-
+    //Get the list of requests that will be made through the HTTP client
     public static UserService getUserService() {
         UserService userService = getRetrofit().create(UserService.class);
 
