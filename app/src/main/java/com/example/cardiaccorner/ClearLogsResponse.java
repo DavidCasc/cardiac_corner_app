58b881170aff3609ac196e3c8e5837d3c214303d
DavidCasc <50248611+DavidCasc@users.noreply.github.com>
2022-04-01 21:04:13 -0400

    Added documentation

--- app/src/main/java/com/example/cardiaccorner/ClearLogsResponse.java
+++ app/src/main/java/com/example/cardiaccorner/ClearLogsResponse.java
@@ -1,4 +1,10 @@
 package com.example.cardiaccorner;
 
+/**
+ * This is a blank class that is needed for the API call to clear
+ * logs... Retrofit2 can be weird with its calls
+ *
+ * @Author: David Casciano
+ */
 public class ClearLogsResponse {
 }
